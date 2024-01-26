9f441bf6a95fdaefe898aa4362a971fe009b1e66
Mega Turbik <turbik_programmist@mail.ru>
2024-01-26 20:56:27 +0700

    Турбик сделал имена 😎

---
+++ Assets/Scripts/TurboName.cs
@@ -0,0 +1,24 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class TurboName : NetworkBehaviour
+{
+    [SerializeField] TMP_Text turboNameText;
+
+    [SyncVar(hook = nameof(HandlePlayerNameUpdated))]
+    string turboName;
+    
+    void HandlePlayerNameUpdated(string oldText, string newText)
+    {
+        turboNameText.text = newText;
+    }
+
+    public override void OnStartServer()
+    {
+        turboName = $"ТУРБИК {connectionToClient.connectionId}";
+    }
+}
